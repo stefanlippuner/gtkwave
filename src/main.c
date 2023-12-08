db86a926660c29796450ea428f7a96ee22d38a9c
Ralf Fuest <mail@rfuest.de>
2023-12-08 18:34:16 +0100

    Convert VcdFile into GObject

--- src/main.c
+++ src/main.c
@@ -67,6 +67,7 @@
 #include "ttranslate.h"
 #include "signal_list.h"
 #include "gw-time-display.h"
+#include "gw-vcd-file.h"
 
 #include "tcl_helper.h"
 #if defined(HAVE_LIBTCL)
@@ -1714,7 +1715,7 @@ loader_check_head:
 
                 switch (GLOBALS->is_lx2) {
                     case LXT2_IS_VLIST:
-                        vcd_import_masked(GLOBALS->vcd_file);
+                        gw_vcd_file_import_masked(GW_VCD_FILE(GLOBALS->dump_file));
                         break;
                     case LXT2_IS_FST:
                         fst_import_masked(GLOBALS->fst_file);
